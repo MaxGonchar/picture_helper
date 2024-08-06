adf505b5715c311cfedb710aaf220069e647d6b1
MaxGonchar <boborodada@gmail.com>
2024-08-07 00:02:39 +0300

    add validator sceleton

---
+++ validator.py
@@ -0,0 +1,9 @@
+# get next unsorted image
+# generate URL
+# provide URL, is_good, likelihood to user
+# get user feedback
+# in transaction:
+    # store image to sorted
+    # update sorted tags
+    # remove image from unsorted
+# provide next image if user wants to continue or image exists
